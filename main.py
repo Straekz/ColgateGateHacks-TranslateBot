43b90fe45a5188f3c9da372630b1709953a656a6
m-sef <seth.l.moore0506@gmail.com>
2024-09-29 08:47:49 -0400

    Backseat gamer...

--- main.py
+++ main.py
@@ -1,35 +1,44 @@
-import schedule, time
+import threading
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 import pyautogui
-import pyttsx3
-
-engine = pyttsx3.init()
+from gtts import gTTS
+import pyglet
 
 load_dotenv()
 
 GEMINI_API = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key = GEMINI_API)
 
-engine = pyttsx3.init()
+screenshot_file_path = "tmp/temp.png"
+audio_file_path = "tmp/temp.mp3"
+prompt = "You are a backseat gamer. Playfully suggest something I should do. Please, keep your response minimal and under 1 sentences."
 
 def commentary():
-    screenshot = pyautogui.screenshot("screenshot.png")
+    threading.Timer(6.0, commentary).start()
+    print("Generating commentary...")
+    screenshot = pyautogui.screenshot(screenshot_file_path)
 
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content([
-        genai.upload_file("screenshot.png"),
-        "\n\n",
-        "You are my professional gaming coach, who is also Goku from Dragon Ball Z. Tell me how I can improve on my current situation in under 3 sentences."
-    ])
+    try:
+        model = genai.GenerativeModel("gemini-1.5-flash")
+        response = model.generate_content([
+            genai.upload_file(screenshot_file_path),
+            prompt
+        ])
+    except:
+        return
 
-    engine.say(response.text)
-    engine.runAndWait()
+    text_to_speech = gTTS(
+        text = response.text,
+        lang = 'en'
+    )
+    text_to_speech.save(audio_file_path)
+
+    audio = pyglet.media.load(audio_file_path, streaming=False)
+    audio.play().pitch = 1.3
 
 if __name__ == "__main__":
-    schedule.every(5.0).seconds.do(commentary)
+    commentary()
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    pyglet.app.run()
