93ace93dd5f48854c86fa22d639494724107eea6
m-sef <seth.l.moore0506@gmail.com>
2024-09-28 22:25:00 -0400

    Added gemini text generation

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import discord
+import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-load_dotenv()
+genai.configure(api_key = os.getenv("GEMINI"))
 
 @client.event
 async def on_ready():
@@ -18,7 +21,9 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$'):
+        model = genai.GenerativeModel("gemini-1.5-flash")
+        response = model.generate_content(message.content[1:] + " Ensure that your response is less than 1000 characters in length.")
+        await message.channel.send(response.text)
 
-client.run(os.getenv('TOKEN'))
+client.run(os.getenv('DISCORD_BOT_TOKEN'))
