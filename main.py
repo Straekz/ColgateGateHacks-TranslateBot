2a086d733826e984060de625303341fd5f74abe4
m-sef <seth.l.moore0506@gmail.com>
2024-09-29 03:45:43 -0400

    Removed discord functionality

--- main.py
+++ main.py
@@ -1,58 +1,35 @@
-import logging
-import discord
-from discord import app_commands
-from discord.ext import tasks, commands
+import schedule, time
 import google.generativeai as genai
-import os
 from dotenv import load_dotenv
+import os
 import pyautogui
 import pyttsx3
 
+engine = pyttsx3.init()
+
 load_dotenv()
 
-DISCORD_API = os.getenv('DISCORD_BOT_TOKEN')
-GEMINI_API = os.getenv("GEMINI")
-
-bot = commands.Bot(command_prefix = '@@', intents = discord.Intents.default())
-
+GEMINI_API = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key = GEMINI_API)
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(filename='genai.log', level=logging.INFO)
 
 engine = pyttsx3.init()
 
-@tasks.loop(seconds = 8.0)
-async def commentary():
+def commentary():
     screenshot = pyautogui.screenshot("screenshot.png")
 
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content([
         genai.upload_file("screenshot.png"),
         "\n\n",
-        "You are my gaming coach. Tell me what you see in only 1 sentence, then tell me how I can improve in my current situation in only 1 sentence."
+        "You are my professional gaming coach, who is also Goku from Dragon Ball Z. Tell me how I can improve on my current situation in under 3 sentences."
     ])
 
     engine.say(response.text)
     engine.runAndWait()
 
-@bot.event
-async def on_ready():
-    logger.info("Ready!")
-    try:
-        synced = await bot.tree.sync()
-        logger.info(f"Synced {len(synced)} command(s)")
-    except Exception as error:
-        logger.error(error)
-    commentary.start()
+if __name__ == "__main__":
+    schedule.every(5.0).seconds.do(commentary)
 
-@bot.tree.command(name = "prompt")
-@app_commands.describe(content = "Prompt to send to Gemini")
-async def prompt(interaction : discord.Interaction, content : str):
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content(
-        f"Translate the following content to Japanese: {content}"
-    )
-    await interaction.response.send_message(response.text, ephemeral=True)
-
-bot.run(DISCORD_API)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
