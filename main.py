85f5d69b6761df3b5196e1cd7785c1e4acd7342f
Tyrone Xue <skittishsky@gmail.com>
2024-09-28 23:03:38 -0400

    Commands

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import discord
+from discord.ext import commands
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
+DISCORD_API = os.getenv('DISCORD_BOT_TOKEN')
+GEMINI_API = os.getenv("GEMINI")
+
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-genai.configure(api_key = os.getenv("GEMINI"))
+genai.configure(api_key = GEMINI_API)
 
 @client.event
 async def on_ready():
@@ -26,4 +31,14 @@ async def on_message(message):
         response = model.generate_content(message.content[1:] + " Ensure that your response is less than 1000 characters in length.")
         await message.channel.send(response.text)
 
-client.run(os.getenv('DISCORD_BOT_TOKEN'))
+@bot.command()
+async def join(ctx):
+    channel = ctx.author.voice.channel
+    await channel.connect()
+
+@bot.command()
+async def leave(ctx):
+    await ctx.voice_client.disconnect()
+
+bot.run(DISCORD_API)
+# client.run(DISCORD_API)
