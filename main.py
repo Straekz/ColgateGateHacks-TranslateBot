8fb2819962a12384292dee83f0b7ec17fa50be3d
m-sef <seth.l.moore0506@gmail.com>
2024-09-29 00:45:09 -0400

    Slash commands and ephemeral messaging

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 import google.generativeai as genai
 import os
@@ -9,27 +10,26 @@ load_dotenv()
 DISCORD_API = os.getenv('DISCORD_BOT_TOKEN')
 GEMINI_API = os.getenv("GEMINI")
 
-intents = discord.Intents.default()
-intents.message_content = True
-
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='$', intents=discord.Intents.default())
 
 genai.configure(api_key = GEMINI_API)
 
-@bot.command()
-async def prompt(ctx, *, msg):
-        model = genai.GenerativeModel("gemini-1.5-flash")
-        response = model.generate_content(msg + " Ensure that your response is less than 1000 characters in length.")
-        await ctx.channel.send(response.text)
+@bot.event
+async def on_ready():
+    print("Ready!")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as error:
+        print(error)
 
-@bot.command()
-async def join(ctx):
-    channel = ctx.author.voice.channel
-    await channel.connect()
-
-@bot.command()
-async def leave(ctx):
-    await ctx.voice_client.disconnect()
+@bot.tree.command(name="prompt")
+@app_commands.describe(content = "Prompt to send to Gemini")
+async def prompt(interaction : discord.Interaction, content : str):
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(
+        "You are impersonating spongebob. " + content + " Ensure that your response is less than 1000 characters in length."
+    )
+    await interaction.response.send_message(response.text, ephemeral=True)
 
 bot.run(DISCORD_API)
-# client.run(DISCORD_API)
